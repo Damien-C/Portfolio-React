d7cf0fa9d5479d8ee0dba2a0afa2a0029ce8aba5
Damien-C <cmshkn@gmail.com>
2020-08-18 16:15:11 +0900

    Achievement page layout

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -1,15 +1,86 @@
 import React from 'react';
 import '../App.css';
+import { useSwipeable, Swipeable } from 'react-swipeable';
+
+
+
 
 class Achievements extends React.Component{
     render(){
+        
         return(
             <div className='frameWrapMain'>
-                <div className='achievementsText pageTitle'>
+                <div className='contentWrapper'>
+                    <div className='achievementsText pageTitle'>
                     "Genius is one percent inspiration and ninety-nine perspiration."
+                    </div>
+                    
+
+
                 </div>
 
+
+
+                <div className='photoListWrap'>
+                        <div className='photoList'>
+                            <div className='photoItem'>
+                                <div className='photo'>
+                                
+                                </div>
+                                <div className='photoDesc'>
+                                    project
+                                </div>
+                            </div>
+
+                            <div className='photoItem'>
+                                <div className='photo'>
+                                
+                                </div>
+                                <div className='photoDesc'>
+                                    project
+                                </div>
+                            </div>
+
+                            <div className='photoItem'>
+                                <div className='photo'>
+                                
+                                </div>
+                                <div className='photoDesc'>
+                                    project
+                                </div>
+                            </div>
+
+                            <div className='photoItem'>
+                                <div className='photo'>
+
+                                </div>
+                                <div className='photoDesc'>
+                                    project
+                                </div>
+                            </div>
+
+                            <div className='photoItem'>
+                                <div className='photo'>
+                                
+                                </div>
+                                <div className='photoDesc'>
+                                    project
+                                </div>
+                            </div>
+
+                            <div className='photoItem'>
+                                <div className='photo'>
+                                
+                                </div>
+                                <div className='photoDesc'>
+                                    project
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
             </div>
+            
         );
     }
 }
